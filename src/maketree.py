385b0b2e6f34cbd677033c4b09e763b405ca4424
Chris P <sahib@online.de>
2013-04-25 13:53:29 +0200

    Geht jetzt. Benutzt jetzt das set nur zum deduplizieren und sonst eine liste

--- src/maketree.py
+++ src/maketree.py
@@ -2,8 +2,8 @@
 # encoding: utf-8
 
 
+# Drawing with graphviz
 import pydot as dot
-from itertools import permutations
 
 
 class Document:
@@ -28,23 +28,14 @@ class Distance:
         self.b = b
         self.dist = dist
 
-    def __gt__(self, node):
-        return self.dist > node.dist
-
     def __lt__(self, node):
         return self.dist < node.dist
 
-    def __ge__(self, node):
-        return self.dist >= node.dist
-
-    def __le__(self, node):
-        return self.dist <= node.dist
-
     def __eq__(self, node):
-        return self.dist == node.dist
+        return (self.a is node.a or self.a is node.b) and (self.b is node.a or self.b is node.b)
 
     def __hash__(self):
-        return id(a) + id(b) + self.dist
+        return id(a) + id(b)
 
     def __repr__(self):
         return "Dist({a}-{b}={d})".format(d=self.dist, a=self.a, b=self.b)
@@ -68,28 +59,32 @@ if __name__ == '__main__':
             Cluster([docs[4]], name="E")
     ]
 
-    s = set()
+    s = list()
+
     a, b, c, d, e = cluster
-    s.add(Distance(2, a, b))
-    s.add(Distance(4, a, c))
-    s.add(Distance(5, a, d))
-    s.add(Distance(7, a, e))
+    s.append(Distance(2, a, b))
+    s.append(Distance(4, a, c))
+    s.append(Distance(5, a, d))
+    s.append(Distance(7, a, e))
 
-    s.add(Distance(3, b, c))
-    s.add(Distance(6, b, d))
-    s.add(Distance(8, b, e))
+    s.append(Distance(3, b, c))
+    s.append(Distance(6, b, d))
+    s.append(Distance(8, b, e))
 
-    s.add(Distance(1, c, d))
-    s.add(Distance(2, c, e))
+    s.append(Distance(1, c, d))
+    s.append(Distance(2, c, e))
 
-    s.add(Distance(1, d, e))
+    s.append(Distance(1, d, e))
 
-    print(len(s))
+    # Initial sort
+    s.sort(reverse=True)
 
     # Iterate till n-1 new nodes have been created.
     for i in range(len(cluster) - 1):
         # Get the cluster with the shortest distance out
-        print(s, end='')
+        # On every iteration the length should be sum(0..n-1)
+        print('len(s) = ', len(s), s, end='')
+
         nearest = s.pop()
         left, right = nearest.a, nearest.b
 
@@ -99,24 +94,20 @@ if __name__ == '__main__':
                 name=''.join((left.name, right.name))
         )
 
-        print(' ->', parent.name)
+        print(' ->', left.name + "-" + right.name)
 
-        # Update set with the new parent node
         for dist in s:
-            # Set a to parent if it is in there
             if dist.a is left or dist.a is right:
                 dist.a = parent
-            # Otherwise search dist.b
             elif dist.b is left or dist.b is right:
                 dist.b = parent
-            # elsewhise leave it as it is.
 
         # Remember to draw the edge
         graph.add_edge(dot.Edge(parent.name, left.name))
         graph.add_edge(dot.Edge(parent.name, right.name))
 
-        # Not entirely sure if this is needed
-        s = set(list(s))
+        # Not exactly sure if this is needed
+        s = sorted(set(s), reverse=True)
 
     graph.write_png('cluster.png')
 
